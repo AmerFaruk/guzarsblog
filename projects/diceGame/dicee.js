454827aa34ecae2354194ea78ec12b64f57bac5b
Ömer Faruk Horat <106451250+AmerFaruk@users.noreply.github.com>
2023-07-17 01:50:54 +0300

    Starting Uploads 3

--- projects/diceGame/dicee.js
+++ projects/diceGame/dicee.js
@@ -9,10 +9,11 @@ function roll(){
     document.getElementsByClassName("img1")[1].src="images/d20d"+randomNumberd20_2+".png";
     
     if(randomNumberd20_1>randomNumberd20_2){
-        document.querySelector("h1").innerHTML="Player 1 Won!!";
+        $("h1").text("Player 1 Won!!");
+
     } else if(randomNumberd20_1<randomNumberd20_2){
-        document.querySelector("h1").innerHTML="Player 2 Won!!";
+        $("h1").text("Player 2 Won!!");
     } else {
-        document.querySelector("h1").innerHTML="DRAW!";
+        $("h1").text("DRAW!");
     }
 }
