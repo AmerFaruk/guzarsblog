36b130cc9da8121dc485dc15a8fc0a3930a86b27
OMER FARUK HORAT <omerfarukhrt@gmail.com>
2023-07-27 03:06:25 +0300

    27.07

--- README.md
+++
@@ -1,2 +0,0 @@
-# guzarsblog
-My Blog - Website
--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log('listening song on port 5000');
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const app = express();
+//my selected port
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/about", (req, res) => {
+    res.sendFile(__dirname + "/about-me.html");
+});
+
+app.get("/archive", (req, res) => {
+    res.sendFile(__dirname + "/archive.html");
+});
+
+app.get("/cv", (req, res) => {
+    res.sendFile(__dirname + "/cv.html");
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server started on ${port}`);
+});
